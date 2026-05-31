08558923b453abbd8c0666b78fdcbb12b038d86a
Thiago <thiagopcosta08@gmail.com>
2026-05-31 14:05:36 -0300

    peixe, telas

--- canvas.js
+++ canvas.js
@@ -17,23 +17,45 @@ var peixeX = 100;
 var velocidadePeixe = 5;
 var peixey = 100;
 var velocidadePeixeY = 3;
-
 var gravidade = 0.5;
+var colidiu = false;
+var pegou = false;
+var imgTraira = new Image();
+imgTraira.src = "imagens/trairadojogo.png";
+
+function peixe(x, y) {
+    c.drawImage(imgTraira, x, y, 100, 60);
+}
+
+function perdeu() {
+    window.location.href = "gameover.html";
+}
+
+function venceu() {
+    window.location.href = "venceu.html";
+}
 
 function animate() {
+
+
    animationId = requestAnimationFrame(animate);
    c.clearRect(0, 0, canvas.width, canvas.height);
    c.fillStyle = 'blue';
+
    c.fillRect(0, canvas.height - 50, canvas.width, 50);
    c.fillStyle = 'red';
+
    c.fillRect(peixeX, canvas.height - 55, 30, 30);
    peixeX += velocidadePeixe;
-   c.fillStyle = 'green';
+   c.fillStyle = 'red';
+
    c.fillRect(peixey, canvas.height - 55, 30, 30);
     peixey += velocidadePeixeY;
+
     if (peixeX + 30 > canvas.width || peixeX < 0) {
         velocidadePeixe = -velocidadePeixe;
     }
+
   if (peixey + 30 > canvas.width || peixey < 0) {
         velocidadePeixeY = -velocidadePeixeY;
     }
@@ -62,8 +84,43 @@ function animate() {
         dy = 0;
         dx = -1;
     }
- 
-    
+    if (
+    x + raio > peixeX &&
+    x - raio < peixeX + 30 &&
+    y + raio > canvas.height - 55 &&
+    y - raio < canvas.height - 25
+) {
+    colidiu = true;
+} else {
+    colidiu = false;
+}
+if (pegou && x < 20) {
+
+    c.font = "50px Arial";
+    c.fillStyle = "yellow";
+   venceu();
+
+} 
+if (x < 20 && !pegou) {
+        perdeu();
+    }
+   if (pegou) {
+peixe(300, 100);
+  c.fillText("VOCÊ PEGOU O PEIXE!", 200, 200);
+}
+
 }
 
 animate();
+canvas.addEventListener("click", function(event){
+
+    if (colidiu) {
+        pegou = true;
+      
+       
+
+        
+    }
+
+});
+
